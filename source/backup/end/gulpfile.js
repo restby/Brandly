c85078f633288b2b469d84a92295ae07bdb37675
Anton Tolstenko <restby@gmail.com>
2018-12-14 17:32:48 +0300

    доделать PP

--- source/backup/end/gulpfile.js
+++ source/backup/end/gulpfile.js
@@ -77,9 +77,9 @@ gulp.task("images", function() {
   .pipe(gulp.dest("source/img/"));
 });
 gulp.task("webp", function() {
-  return gulp.src("source/img/**/*.{png,jpg}")
+  return gulp.src("build/img/**/*.{png,jpg}")
   .pipe(webp({quality: 90}))
-  .pipe(gulp.dest("source/img/"));
+  .pipe(gulp.dest("build/img/"));
 });
 gulp.task("server", function () {
   server.init({
